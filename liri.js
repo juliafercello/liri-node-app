6749dae287157d708f8ac4084c2f4c8e6a4a36b1
Julia F <46096629+juliafercello@users.noreply.github.com>
2019-03-30 21:06:56 -0500

    working app

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 //include packages
 var Spotify = require('node-spotify-api');
 var moment = require("moment");
-var axios = require("axios"); 
+var axios = require("axios");
 var fs = require("fs");
 
 //Import Keys for Spotify using dotenv
@@ -10,50 +10,121 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
 //Global Vars for the command and search parameter
-var command = process.argv[2]; 
+var command = process.argv[2];
 var search = process.argv.slice(3).join(" ");
+var movieSearch = false;
 
-//"Movie This"
-if (command === "movie-this") {
-    if (!search){
-        search = "Mr. Nobody"
+//function to build search URL
+function buildSearchURL() {
+    if (movieSearch) {
+        var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
     }
-   var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy"; 
-   axios.get(queryUrl)
-   .then(function (response) {
+    else {
+        var queryUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
+    }
+    getResults(queryUrl);
+}
+
+//Function using Axios to GET movies and concerts
+function getResults(queryUrl) {
+    axios.get(queryUrl)
+        .then(function (response) {
+            if (movieSearch) {
+                displayMovieInfo(response)
+            }
+            else {
+                displayConcertInfo(response);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+//Display Movie Info
+function displayMovieInfo(response) {
     console.log("Title: " + JSON.stringify(response.data.Title, null, 2));
     console.log("Released: " + JSON.stringify(response.data.Year, null, 2));
     console.log("IMDB Rating: " + JSON.stringify(response.data.imdbRating, null, 2));
-
     //Find rotten tomatoes better
     console.log("Rotten Tomatoes: " + JSON.stringify(response.data.Ratings[1].Value, null, 2));
     console.log("Country: " + JSON.stringify(response.data.Country, null, 2));
     console.log("Language: " + JSON.stringify(response.data.Language, null, 2));
     console.log("Plot: " + JSON.stringify(response.data.Plot, null, 2));
     console.log("Actors: " + JSON.stringify(response.data.Actors, null, 2));
-   })
-   .catch(function (error) {
-     console.log(error);
-   });
 }
 
-// concert-this
-// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY")
+//Display Concert Info
+function displayConcertInfo(response) {
+    if (response.data.length > 0) {
+        console.log(search + " is playing here: ");
+        console.log("*****************************************************")
+        for (var i = 0; i < response.data.length; i++) {
+            console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY") +
+                "\nVenue: " + response.data[i].venue.name + " \nLocation: " + response.data[i].venue.city + " " +
+                response.data[i].venue.region + " " + response.data[i].venue.country)
+            console.log("*****************************************************")
+        }
+    }
+    else {
+        console.log("Sad News - no concerts were found for " + search);
+    }
+}
 
-// spotify-this-song
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
+//Search spotify for song and display results 
+function getSong() {
+    spotify.search({ type: 'track', query: search, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("Artists: " + data.tracks.items[0].artists[0].name); //DO THIS BETTER
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+    });
+}
 
-// do-what-it-says
-// Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-// It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-// Edit the text in random.txt to test out the feature for movie-this and concert-this.
+//Evaluate user input and search for applicable movie, band, or song.
+if (command === "movie-this") {
+    if (!search) {
+        search = "Mr. Nobody"
+    }
+    movieSearch = true;
+    buildSearchURL();
+}
+else if (command === "concert-this") {
+    if (!search) {
+        search = "NKOTB"
+    }
+    buildSearchURL();
+}
+else if (command === "spotify-this-song") {
+    if (!search) {
+        search = "The Sign";
+    }
+    getSong();
+}
+else if (command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log("Unable to do what it says")
+        }
+        var random = data.split(",");
+        search = random.slice(1).join(" ");
+        search = search.replace(/"/g,"");
+
+        if (random[0] === "movie-this") {
+            movieSearch = true;
+            buildSearchURL();
+        }
+        else if (random[0] === "concert-this") {
+            buildSearchURL();
+        }
+        else {
+            getSong();
+        }
+    });
+}
 
 // Bonus
 // In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
